ff1cfb361828bace7ddb553b20afdb1a4055b207
veerankirajesh <adevops87@gmail.com>
2024-09-24 16:18:49 +0530

    added 2nd script

--- catalogue.sh
+++ catalogue.sh
@@ -95,5 +95,4 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "Installing MongoDB client"
 
 mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
-
 VALIDATE $? "Loading catalouge data into MongoDB"
