71866411ef72c3e5c3773288a936b429ab0c554b
veerankirajesh <adevops87@gmail.com>
2024-09-24 15:33:01 +0530

    added 2nd script

--- catalogue.sh
+++ catalogue.sh
@@ -29,12 +29,19 @@ then
 else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
+
 dnf module disable nodejs -y &>> $LOGFILE
+
 VALIDATE $? "Disabling current NodeJS"
+
 dnf module enable nodejs:18 -y  &>> $LOGFILE
+
 VALIDATE $? "Enabling NodeJS:18"
+
 dnf install nodejs -y  &>> $LOGFILE
+
 VALIDATE $? "Installing NodeJS:18"
+
 id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
@@ -43,27 +50,50 @@ then
 else
     echo -e "roboshop user already exist $Y SKIPPING $N"
 fi
+
 mkdir -p /app
+
 VALIDATE $? "creating app directory"
+
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $LOGFILE
+
 VALIDATE $? "Downloading catalogue application"
+
 cd /app 
+
 unzip -o /tmp/catalogue.zip  &>> $LOGFILE
+
 VALIDATE $? "unzipping catalogue"
+
 npm install  &>> $LOGFILE
+
 VALIDATE $? "Installing dependencies"
+
 # use absolute, because catalogue.service exists there
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
 VALIDATE $? "Copying catalogue service file"
+
 systemctl daemon-reload &>> $LOGFILE
+
 VALIDATE $? "catalogue daemon reload"
+
 systemctl enable catalogue &>> $LOGFILE
+
 VALIDATE $? "Enable catalogue"
+
 systemctl start catalogue &>> $LOGFILE
+
 VALIDATE $? "Starting catalogue"
+
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
 VALIDATE $? "copying mongodb repo"
+
 dnf install mongodb-org-shell -y &>> $LOGFILE
+
 VALIDATE $? "Installing MongoDB client"
-mongo --host mongodb.rajesh76.online </app/schema/catalogue.js &>> $LOGFILE
+
+mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+
 VALIDATE $? "Loading catalouge data into MongoDB"
