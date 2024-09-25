e61895ca67d324860bc3d801288a52f1d95de85e
veerankirajesh <adevops87@gmail.com>
2024-09-25 23:41:55 +0530

    added 2nd script

--- web.sh
+++ web.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+MONGDB_HOST=mongodb.daws76s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -31,28 +31,37 @@ else
 fi # fi means reverse of if, indicating condition end
 
 dnf install nginx -y &>> $LOGFILE
+ 
 VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>> $LOGFILE
+
 VALIDATE $? "Enable nginx" 
 
 systemctl start nginx &>> $LOGFILE
+
 VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+
 VALIDATE $? "removed default website"
 
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
+
 VALIDATE $? "Downloaded web application"
 
 cd /usr/share/nginx/html &>> $LOGFILE
+
 VALIDATE $? "moving nginx html directory"
 
 unzip -o /tmp/web.zip &>> $LOGFILE
-VALIDATE $? "unzipping web"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+VALIDATE $? "unzipping web"
+ 
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE 
+
 VALIDATE $? "copied roboshop reverse proxy config"
 
 systemctl restart nginx &>> $LOGFILE
+
 VALIDATE $? "restarted nginx"
