f716d438337c2a6f50189e805ba6495ebf1f1650
veerankirajesh <adevops87@gmail.com>
2024-09-25 20:29:20 +0530

    added 2nd script

--- cart.sh
+++ cart.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-exec &>$LOGFILE
+
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -17,6 +21,7 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
@@ -24,14 +29,18 @@ then
 else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
-dnf module disable nodejs -y
+
+dnf module disable nodejs -y &>> $LOGFILE
+
 VALIDATE $? "Disabling current NodeJS"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
 VALIDATE $? "Enabling NodeJS:18"
 
-dnf install nodejs -y
-VALIDATE $? "install nodejs:18"
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Installing NodeJS:18"
 
 id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
@@ -41,28 +50,38 @@ then
 else
     echo -e "roboshop user already exist $Y SKIPPING $N"
 fi
-mkdir -p /app
-VALIDATE $? "createing app directory"
 
-curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
-VALIDATE $? "downloading cart application"
+mkdir -p /app
+
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip  &>> $LOGFILE
+
+VALIDATE $? "Downloading cart application"
 
 cd /app 
 
 unzip -o /tmp/cart.zip  &>> $LOGFILE
+
 VALIDATE $? "unzipping cart"
 
-npm install 
+npm install  &>> $LOGFILE
+
 VALIDATE $? "Installing dependencies"
 
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+# use absolute, because cart.service exists there
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+
 VALIDATE $? "Copying cart service file"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
+
 VALIDATE $? "cart daemon reload"
 
-systemctl enable cart 
+systemctl enable cart &>> $LOGFILE
+
 VALIDATE $? "Enable cart"
 
-systemctl start cart
+systemctl start cart &>> $LOGFILE
+
 VALIDATE $? "Starting cart"
